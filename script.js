aec833a7662907af8abe31be550eb9f8c6e781ba
Joilson Vieira <joilsontil@gmail.com>
2025-07-09 00:09:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const botao = document.getElementById('botaoComecar');
+
+    if (botao) {
+        botao.addEventListener('click', function() {
+            window.location.href = 'https://html5up.net/uploads/demos/forty/#one';
+        });
+    }
+});
